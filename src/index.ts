72d9b8dab23625340e71782f9b639092099a3010
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2025-12-16 13:58:48 -0800

    HTML

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,24 @@ import { createWish, deleteWish, fulfillWish, listWishes } from "./db/queries"
 const app = new Hono()
 
 app.get('/', (c) => {
-  return c.text('Beans!')
+  return c.html(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landing</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: black;
+            height: 100vh;
+        }
+    </style>
+</head>
+<body>
+</body>
+</html>`)
 })
 
 app.get("/api/wishes", (c) => c.json(listWishes()))
